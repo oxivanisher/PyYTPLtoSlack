bf1243f895440602fc7a1f4f60db37f81d098113
Marc Urben <aegnor@mittelerde.ch>
2020-01-29 16:56:14 +0100

    ... google auth is broken

--- README.md
+++ README.md
@@ -3,9 +3,10 @@ This is a collection of YouTube helper scripts.
 
 ## ytFetchPlaylist.py
 This script fetches all video ids from a Playlist and saves them to a json file usable by the bot scripts in this
-repository. Call it with `python ytFetchPlaylist.py PLAYLIST_ID`  
+repository. Call it with `python ytFetchPlaylist.py PLAYLIST_ID`
 **Important:** You have to create a [OAuth 2.0 Client ID](https://console.developers.google.com/apis/credentials) and
-store the .json as `client_secret.json` to use this script.
+store the .json as `client_secret.json` to use this script. Also you have to add the `../auth/youtube.readonly` scope
+by going to the `OAuth consent screen` section and `Add scope`. 
 
 ## planned :D
 A very simple bot to post a youtube playlist to a slack channel (It is Wednesday my dudes!)
--- ytFetchPlaylist.py
+++ ytFetchPlaylist.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 # Sample Python code for youtube.channels.list
@@ -5,38 +6,91 @@
 # https://developers.google.com/explorer-help/guides/code_samples#python
 
 import os
+import json
+import logging
+import pickle
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
+logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+# import google_auth_oauthlib.flow
+# import googleapiclient.discovery
+# import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 
-def fetchVideos(youtube, playlistId, pageToken=None):
+# thanks https://github.com/googleapis/google-api-python-client/issues/325
+# import hashlib
+# import tempfile
+#
+# class DiscoveryCache:
+#     def filename(self, url):
+#         return os.path.join(os.getcwd(), 'google_api_discovery_' + hashlib.md5(url.encode()).hexdigest())
+#
+#     def get(self, url):
+#         try:
+#             with open(self.filename(url), 'rb') as f:
+#                 return f.read().decode()
+#         except FileNotFoundError:
+#             return None
+#
+#     def set(self, url, content):
+#         with tempfile.NamedTemporaryFile(delete=False) as f:
+#             f.write(content.encode())
+#             f.flush()
+#             os.fsync(f)
+#         os.rename(f.name, self.filename(url))
+
+
+def fetch_videos(youtube, playlist_id, next_page_token=None):
+    logging.debug("Fetching data from youtube...")
     request = youtube.playlistItems().list(
         part="id",
         maxResults=10,
-        playlistId=playlistId,
-        pageToken=pageToken
+        playlistId=playlist_id,
+        pageToken=next_page_token
     )
     return request.execute()
 
 
-def main():
+def main(playlist_id):
+    logging.info("Starting to fetch video ids from YouTube playlist: %s" % playlist_id)
+    creds = None
+
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'client_secret.json', scopes)
+            # creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
-    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    # os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "client_secret.json"
-    playlistId = "PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8"
+    video_ids = []
 
     # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
-    credentials = flow.run_console()
-    youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+    # flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    # credentials = flow.run_console()
+    # youtube = build(api_service_name, api_version, credentials=creds, cache=DiscoveryCache())
+    youtube = build(api_service_name, api_version, credentials=creds)
 
     # request = youtube.channels().list(
     #     part="snippet,contentDetails,statistics",
@@ -47,18 +101,29 @@ def main():
     #
     # print(response)
 
-    result = fetchVideos(youtube, playlistId)
+    result = fetch_videos(youtube, playlist_id)
     videos = result['items']
-    print (result)
+    print(result)
     if "nextPageToken" in result.keys():
         while True:
-            result = fetchVideos(youtube, playlistId, result['nextPageToken'])
+            result = fetch_videos(youtube, playlist_id, result['nextPageToken'])
             videos.extend(result['items'])
             if "nextPageToken" not in result.keys():
                 break
 
     print(videos)
+    for item in videos:
+        if item['kind'] == "youtube#playlistItem":
+            video_ids.append(item['id'])
+
+    if len(video_ids):
+        filename = "yt-playlist-video-ids-%s.json" % playlist_id
+        with open(filename, 'w') as outfile:
+            json.dump(video_ids, outfile)
+        logging.info("Wrote %s video IDs to file %s" % (len(video_ids), filename))
+    else:
+        logging.warning("No video IDs found.")
 
 
 if __name__ == "__main__":
-    main()
+    main("PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8")
