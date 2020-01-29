ddb3fa7ac37e25a6be54244fe99d891e2c6cd68d
Marc Urben <aegnor@mittelerde.ch>
2020-01-29 09:42:52 +0100

    renaming things

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# PyYTPLtoSlack
+# YouTube-Scripts
+This is a collection of YouTube helper scripts.
+
+## ytFetchPlaylist.py
+This script fetches all video ids from a Playlist and saves them to a json file usable by the bot scripts in this
+repository. Call it with `python ytFetchPlaylist.py PLAYLIST_ID`  
+**Important:** You have to create a [OAuth 2.0 Client ID](https://console.developers.google.com/apis/credentials) and
+store the .json as `client_secret.json` to use this script.
+
+## planned :D
 A very simple bot to post a youtube playlist to a slack channel (It is Wednesday my dudes!)
--- ytFetchPlaylist.py
+++ ytFetchPlaylist.py
@@ -12,6 +12,17 @@ import googleapiclient.errors
 
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+
+def fetchVideos(youtube, playlistId, pageToken=None):
+    request = youtube.playlistItems().list(
+        part="id",
+        maxResults=10,
+        playlistId=playlistId,
+        pageToken=pageToken
+    )
+    return request.execute()
+
+
 def main():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
@@ -20,6 +31,7 @@ def main():
     api_service_name = "youtube"
     api_version = "v3"
     client_secrets_file = "client_secret.json"
+    playlistId = "PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8"
 
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
@@ -35,14 +47,17 @@ def main():
     #
     # print(response)
 
-    request = youtube.playlistItems().list(
-        part="id",
-        maxResults=100,
-        playlistId="PLy3-VH7qrUZ5IVq_lISnoccVIYZCMvi-8"
-    )
-    response = request.execute()
+    result = fetchVideos(youtube, playlistId)
+    videos = result['items']
+    print (result)
+    if "nextPageToken" in result.keys():
+        while True:
+            result = fetchVideos(youtube, playlistId, result['nextPageToken'])
+            videos.extend(result['items'])
+            if "nextPageToken" not in result.keys():
+                break
 
-    print(response)
+    print(videos)
 
 
 if __name__ == "__main__":
